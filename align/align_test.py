118c31a26dc26cc84714dde4825f79b6305647be
martinstrn <144115161+martinstrn@users.noreply.github.com>
2024-01-16 16:26:23 +0100

    Add files via upload

---
+++ align/align_test.py
@@ -0,0 +1,10 @@
+import align
+
+def test_needleman():
+    assert align.needleman("","")==[[[0]],"",""]
+    
+    #exemple simple
+    assert align.needleman("ATA",'AA')[1:]==['ATA', 'A-A']
+
+    #exemple de wikipedia
+    assert align.needleman("GCAGCG","GATTACA")[1:]==['GCA--GCG', 'G-ATTACA']
